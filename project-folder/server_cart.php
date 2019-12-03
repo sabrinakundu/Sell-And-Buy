313c7f776af95d3676e036d68ae4cf040098dda1
zhourw <zhourw@vcu.edu>
2019-12-03 09:46:34 -0500

    added Reagans Branch

---
+++ project-folder/server_cart.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sell or Buy</title>
+</head>
+<style>
+.page_title, .slogan {
+    font-size: 7;
+}
+
+p {
+ text-align:center
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li b{
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li a:hover {
+  background-color: #111;
+}
+</style>
+<body>
+    <ul>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href="selleraccount.php">Seller Account</a></li>
+    <li><a href="cart.html">Cart</a></li>
+    <li><a href="existing_orders.html">Orders</a></li>
+    <li><a href="delivery_driver.html">Driver Account</a></li>
+    <li><a href="account.html">User Account</a></li>
+    </ul>
+    <h1>
+    <p class="page_title">Cart</p>
+    </h1>
+    <?php
+    $host = "35.192.209.221";
+    $user = "root";
+    $password = "1234";
+    $dbname = "sob";
+
+    $conn = mysqli_connect($host, $user, $password, $dbname);
+    if(!$conn) {
+        die ('Could not connect to the database server' . mysqli_connect_error());
+    }
+
+    $user_id =
+    $get_cart = "select p.product_name, c.quantity, p.price, c.cart
+                from cart c join product p on c.product_id = p.product_id
+                where c.user_id = '$user_id'";
+    $query = mysqli_query($conn, $get_cart);
+    while($row = mysqli_fetch_array($query)) {
+      $product_name = $row['product_name'];
+      $quantity = $row['quantity'];
+      $price = $row['price'];
+      $product_id = $row['product_id']
+      echo "<div class='product_box'>
+          <div class='top_prod_box'>
+          <div class='center_prod_box'>
+            <div class='product_name'>$product_name</div>
+            <div class='quantity'>$quantity</div>
+            <div class='price'>$price</div>
+            <form class='register-form' action='server_cart.php' method='POST'>
+            <button type='submit' name='product_id' value = '<?php '$product_id' ?>'>Delete Product</button>
+            </form>
+          </div>
+          </div>
+          </div>";
+          //delete button query
+          $delete = "delete from cart where user_id = $user_id and product_id = '$product_id'";
+          if(isset($_POST['product_id'])) {
+            $delete_query = mysqli_query($conn, $delete);
+          }
+    }
+
+    //order button
+    echo "<form class='register-form' action=server_orders.php' method='POST'>
+    <button type='submit' name='order_button' value = '<?php '$product_id' ?>'>Delete Product</button>
+    </form>"
+
+    $date = new DateTime($input_date);
+    $dateDelivery = $date->modify('+14 day');
+
+    $order = "insert into orders(user_id, order_date, order_status, schduled_delivery)
+              values('$user_id', '$date', 'not delivered', '$dateDelivery')"
+    if(isset($_POST['order_button'])) {
+      $order_query = mysqli_query($conn, $order);
+      //TODO: insert every product
+      $insert_order_products = "insert into order_products(user_id, product_id, quantity)
+                                values (select * from cart where user_id = '$user_id')";
+    }
+
+    //TODO: insert products into order
+
+
+    ?>
+</body>
+<script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+<script>
+  $(.product_box).on("click", function() {
+    $(".pop-overlay, .popup-content").addClass("active");
+  });
+
+  $(".close, .pop-overlay").on("click", function() {
+    $(.popup-overlay, .popup-content).removeClass("active");
+  })
+</script>
+<style>
+  .popup-overlay {
+    visibility: hidden;
+    position: absolute;
+    background: #ffffff;
+    border: 3px solid #666666;
+    width: 50%;
+    height: 50%;
+    left: 25%;
+  }
+  .popup-content {
+    visibility: hidden;
+  }
+  .popup-content.active {
+    visibility: visible;
+  }
+  button {
+    display: inline-block;
+    vertical-align: middle;
+    border-radius: 30px;
+    margin: 0.20rem;
+    font-size: 1rem;
+    color: #666666;
+    background: #ffffff;
+    border: 1px solid #666666;
+  }
+  button:hover {
+    border: 1px solid #666666;
+    background: #666666;
+    color: #ffffff;
+  }
+</style>
+</html>
---
+++ project-folder/server_delivery_orders.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sell or Buy</title>
+</head>
+<style>
+.page_title, .slogan {
+    font-size: 7;
+}
+
+p {
+ text-align:center
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li b{
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li a:hover {
+  background-color: #111;
+}
+</style>
+<body>
+    <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="selleraccount.php">Seller Account</a></li>
+    <li><a href="cart.html">Cart</a></li>
+    <li><a href="existing_orders.html">Orders</a></li>
+    <li><a class="active" href="delivery_driver.html">Driver Account</a></li>
+    <li><a href="account.html">User Account</a></li>
+    </ul>
+    <h1>
+    <p class="page_title">Delivery Orders</p>
+    </h1>
+
+    <?php
+    $host = "35.192.209.221";
+    $user = "root";
+    $password = "1234";
+    $dbname = "sob";
+
+    $conn = mysqli_connect($host, $user, $password, $dbname);
+    if(!$conn) {
+        die ('Could not connect to the database server' . mysqli_connect_error());
+    }
+
+    include 'server_driver.php'
+    $driver_user_id "select user_id from accounts where '$email' = accounts.email";
+    $query_driver_user_id = mysqli_query($conn, $driver_user_id);
+    //$get_order_id = "select order_id from delivers where user_id = $driver_user_id";
+    $get_deliver_info = "select a.street, a.city, a.zip, a.state, o.schduled_delivery
+                        from delivers d join orders o
+                        join accounts a
+                        on o.user_id = a.user_id
+                        where d.user_id = '$query_driver_user_id'";
+    $query = mysqli_query($conn, $get_deliver_info);
+
+    while($row = mysqli_fetch_array($query)) {
+      $street = $row['street'];
+      $city = $row['city'];
+      $zip = $row['zip'];
+      $state = $row['state'];
+      $schduled_delivery = $row['schduled_delivery'];
+      echo "<div class='product_box'>
+          <div class='top_prod_box'>
+          <div class='center_prod_box'>
+            <div class='street'>$street</div>
+            <div class='city'>$city</div>
+            <div class='zip'>$zip</div>
+            <div class='state'>$state</div>
+            <div class='schduled_delivery'>$schduled_delivery</div>
+          </div>
+          </div>
+          </div>";
+    }
+    ?>
+</body>
+<script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+<script>
+  $(.product_box).on("click", function() {
+    $(".pop-overlay, .popup-content").addClass("active");
+  });
+
+  $(".close, .pop-overlay").on("click", function() {
+    $(.popup-overlay, .popup-content).removeClass("active");
+  })
+</script>
+<style>
+  .popup-overlay {
+    visibility: hidden;
+    position: absolute;
+    background: #ffffff;
+    border: 3px solid #666666;
+    width: 50%;
+    height: 50%;
+    left: 25%;
+  }
+  .popup-content {
+    visibility: hidden;
+  }
+  .popup-content.active {
+    visibility: visible;
+  }
+  button {
+    display: inline-block;
+    vertical-align: middle;
+    border-radius: 30px;
+    margin: 0.20rem;
+    font-size: 1rem;
+    color: #666666;
+    background: #ffffff;
+    border: 1px solid #666666;
+  }
+  button:hover {
+    border: 1px solid #666666;
+    background: #666666;
+    color: #ffffff;
+  }
+</style>
+</html>
